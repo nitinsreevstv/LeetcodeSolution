ecb0c7447c00c4093de8f112028c01217330f7bf
nitinsreevstv <nitinsreevstv@yahoo.com>
2024-10-21 16:59:29 +0530

    updated a edge case

--- DSA/FloornumberBinarySearch.java
+++ DSA/FloornumberBinarySearch.java
@@ -1,19 +1,25 @@
 public class FloornumberBinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,3,5,7,9,13,24,33,36,56,77,87,98};
-        int target = 22;
+        int target = 0;
 
         int start = 0, end = arr.length - 1;
-        while(start<=end){
-            int mid = (start + end) / 2;
-            if(arr[mid] == target){
-                System.err.println(arr[mid]);
-            }else if(arr[mid]>target){
-                end = mid - 1;
-            }else{
-               start = mid + 1;
-            }   
+        if(target< arr[start]){
+            System.out.println("The number " + target + " is the lowest");
+        }else{
+            while(start<=end){
+                int mid = (start + end) / 2;
+                if(arr[mid] == target){
+                    System.err.println(arr[mid]);
+                }else if(arr[mid]>target){
+                    end = mid - 1;
+                }else{
+                   start = mid + 1;
+                }   
+            }
+            System.out.println(arr[end]);
         }
-        System.out.println(arr[end]);
+        
+        
     }
 }
